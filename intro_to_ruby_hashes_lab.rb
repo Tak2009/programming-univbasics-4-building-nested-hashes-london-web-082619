911cf4bedf04fccd4a807f38966e4e193deef8c7
Takeshi Kimoto <lnuk2009jp@gmail.com>
2019-08-19 16:40:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -17,7 +17,7 @@ def monopoly_with_third_tier
 	hash = {
 		:railroads => {
 						 :pieces => 4,
-						 :rent_in_dollars => 5
+						 :rent_in_dollars => {key1:, key2:, key3:, key4:}
 					 }
 		}
 
